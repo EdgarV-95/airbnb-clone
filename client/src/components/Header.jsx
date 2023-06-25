c3943547732662e54a60073de3fbfc1f7eadcce1
Edgar Veress <veress.edgar@yahoo.com>
2023-06-25 11:44:31 +0100

    feat: Created login and register pages

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,24 +5,27 @@ export default function Header() {
     <div>
       <header className="flex justify-between">
         <div className="flex items-center gap-1">
-          <a href="" className="-rotate-90">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-8 h-10"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-              />
-            </svg>
-          </a>
-
-          <p className="font-bold text-xl">Ascape</p>
+          <Link to={'/'}>
+            <a href="" className="-rotate-90">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-8 h-10"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+                />
+              </svg>
+            </a>
+          </Link>
+          <Link to={'/'}>
+            <p className="font-bold text-xl">Ascape</p>
+          </Link>
         </div>
         <div className="flex gap-3.5 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
           <div>Anywhere</div>
@@ -30,7 +33,7 @@ export default function Header() {
           <div>Any week</div>
           <div className="border-l border-gray-300"></div>
           <div>Add guests</div>
-          <button className="text-black p-1 rounded-full">
+          <div className="bg-primary text-white p-1 rounded-full cursor-pointer ">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -45,10 +48,10 @@ export default function Header() {
                 d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
               />
             </svg>
-          </button>
+          </div>
         </div>
         <div className="flex items-center gap-3.5 border border-gray-300 rounded-full py-2 px-4">
-          <button>
+          <div className="cursor-pointer">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -63,7 +66,7 @@ export default function Header() {
                 d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
               />
             </svg>
-          </button>
+          </div>
           <Link to={'/login'}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
