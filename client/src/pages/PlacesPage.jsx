30a2dcd6fbf98e9410071348240b6786fb1ce66e
Edgar Veress <veress.edgar@yahoo.com>
2023-07-08 22:57:09 +0100

    feat: Started new listing form. (2:42)

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -38,17 +38,43 @@ export default function PlacesPage() {
               Title for your place
             </p>
             <input type="text" placeholder="Title" />
-            <h2>Address</h2>
+            <h2 className="text-2xl mt-4">Address</h2>
             <p className="text-gray-500 text-sm">
               Address to your place
             </p>
             <input type="text" placeholder="Address" />
             <h2 className="text-2xl mt-4">Photos</h2>
             <p className="text-gray-500 text-sm">More is better</p>
-            <div className="mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6"></div>
-            <button className="border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">
-              +
-            </button>
+            <div className="flex gap-2">
+              <input type="text" placeholder="Add using a link" />
+              <button className="bg-gray-200 px-4 rounded-2xl">
+                Add&nbsp;photo
+              </button>
+            </div>
+            <div className="mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+              <button className="flex justify-center gap-1 border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-8 h-8"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15"
+                  />
+                </svg>
+                Upload
+              </button>
+            </div>
+            <h2 className="text-2xl mt-4">Description</h2>
+            <p className="text-gray-500 text-sm">
+              Description of your place
+            </p>
+            <textarea />
           </form>
         </div>
       )}
